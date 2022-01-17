fc9a10fb5f2370448f7a5be09a3d4b8e2d5022a8
Fedir Katushonok <fedir.katushonok@gmail.com>
2022-01-17 06:54:49 +0200

    Implemented basic communicator

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/CeSiumUA/dopplercli
 
 go 1.17
 
-require github.com/CeSiumUA/dopplerptp v0.0.0-20220116194459-c317e92fb820 // indirect
+require github.com/CeSiumUA/dopplerptp v0.0.0-20220117045218-6d6bf2f8ef21
+
+require (
+	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e // indirect
+	golang.org/x/net v0.0.0-20220114011407-0dd24b26b47d // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"github.com/CeSiumUA/dopplerptp/networking"
+	"os"
+)
 
+func main() {
+	err := networking.StartListener()
+	if err != nil {
+		fmt.Printf("error occured during listener creation: %s", err)
+	}
+	natAddresses, err := networking.GetLocalNetworkAddresses()
+	if err != nil {
+		fmt.Printf("error occured local addresses discovery: %s", err)
+	}
+	networking.ExploreAddresses(natAddresses)
+	fmt.Printf("Enter q to exit:")
+	cliReader := bufio.NewReader(os.Stdin)
+	cmd, err := cliReader.ReadString('\n')
+	if err != nil {
+		fmt.Printf("error occured while reading cmd value: %s", err)
+	}
+	if cmd == "q" {
+		return
+	}
 }
